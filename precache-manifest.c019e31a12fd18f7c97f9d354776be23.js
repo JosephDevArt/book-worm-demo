152244a69415cbd6dbc95c1999ea264526b0fa2f
JosephDevArt <jbdevart@gmail.com>
2020-04-14 21:21:16 +0300

    Updates

---
+++ precache-manifest.c019e31a12fd18f7c97f9d354776be23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfec80a3dcb216f9f9abff8bfb8d0ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad547caa5d5cc21cee3e",
+    "url": "./static/css/main.7bcbc935.chunk.css"
+  },
+  {
+    "revision": "4c6c2829fcd553dff034",
+    "url": "./static/js/2.da4de12a.chunk.js"
+  },
+  {
+    "revision": "0fd9e8d6b85d5a1ec5bf86322bfed999",
+    "url": "./static/js/2.da4de12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad547caa5d5cc21cee3e",
+    "url": "./static/js/main.acf50636.chunk.js"
+  },
+  {
+    "revision": "5952d49d61318f3a705b",
+    "url": "./static/js/runtime-main.6cf5ceee.js"
+  },
+  {
+    "revision": "a6c626c902eb02251426f1fd66f2ecbe",
+    "url": "./static/media/Logo.a6c626c9.svg"
+  },
+  {
+    "revision": "5f1b72774b631cf8d7e575ece5b16a70",
+    "url": "./static/media/sortIcon.5f1b7277.svg"
+  }
+]);
